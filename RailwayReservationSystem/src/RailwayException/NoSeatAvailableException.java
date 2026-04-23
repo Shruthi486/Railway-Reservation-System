5852b900aff6e639388f98a8aa197224b7c2fc06
shrut <shrut@shruthics123>
2026-04-23 21:01:45 +0530

    Initial Commit

---
+++ RailwayReservationSystem/src/RailwayException/NoSeatAvailableException.java
@@ -0,0 +1,11 @@
+package RailwayException;
+
+
+	public class NoSeatAvailableException extends Exception{
+		public NoSeatAvailableException(String msg) {
+			System.out.print(msg);
+		}
+	     
+	}
+
+
